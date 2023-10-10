d5f90195c628e49b187e5fa42a0bb5b85a01a23f
X!v3r <vanniealdamar@gmail.com>
2023-10-10 16:13:53 +0800

    Update windows-install.sh

--- windows-install.sh
+++ windows-install.sh
@@ -17,12 +17,7 @@ sleep 2s
     sleep 2s
 
     # Execute Burp Suite Professional Latest with Key
-    echo 'Executing Burp Suite Professional Latest with Keyloader'
-    echo "java --add-opens=java.desktop/javax.swing=ALL-UNNAMED--add-opens=java.base/java.lang=ALL-UNNAMED --add-opens=java.base/jdk.internal.org.objectweb.asm=ALL-UNNAMED --add-opens=java.base/jdk.internal.org.objectweb.asm.tree=ALL-UNNAMED --add-opens=java.base/jdk.internal.org.objectweb.asm.Opcodes=ALL-UNNAMED -javaagent:$(pwd)/loader.jar -noverify -jar $(pwd)/burpsuite_pro_v$version.jar &" > Burp
-    chmod +x Burp
-    cp Burp /bin/Burp
-    (./Burp)
-echo "java --add-opens=java.desktop/javax.swing=ALL-UNNAMED--add-opens=java.base/java.lang=ALL-UNNAMED --add-opens=java.base/jdk.internal.org.objectweb.asm=ALL-UNNAMED --add-opens=java.base/jdk.internal.org.objectweb.asm.tree=ALL-UNNAMED --add-opens=java.base/jdk.internal.org.objectweb.asm.Opcodes=ALL-UNNAMED -javaagent:loader.jar -noverify -jar burpsuite_pro_latest.jar &" > burpsuite_pro_latest
-chmod +x burpsuite_pro_latest
-cp burpsuite_pro_latest /bin/burpsuite_pro_latest
-./burpsuite_pro_latest
+    echo "java --add-opens=java.desktop/javax.swing=ALL-UNNAMED--add-opens=java.base/java.lang=ALL-UNNAMED --add-opens=java.base/jdk.internal.org.objectweb.asm=ALL-UNNAMED --add-opens=java.base/jdk.internal.org.objectweb.asm.tree=ALL-UNNAMED --add-opens=java.base/jdk.internal.org.objectweb.asm.Opcodes=ALL-UNNAMED -javaagent:loader.jar -noverify -jar burpsuite_pro_latest.jar &" > burpsuite_pro_latest
+    chmod +x burpsuite_pro_latest
+    cp burpsuite_pro_latest /bin/burpsuite_pro_latest
+    ./burpsuite_pro_latest
