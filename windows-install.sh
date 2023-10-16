25568de46385e623a800e2f122920a4054925641
xiv3r <vanniealdamar@gmail.com>
2023-10-16 09:42:29 +0800

    Update windows-install.sh

--- windows-install.sh
+++ windows-install.sh
@@ -1,5 +1,4 @@
-#!/bin/bash
-curl "https://portswigger-cdn.net/burp/releases/download?product=pro&version=&type=jar" -o burpsuite_pro_latest.jar
+#!/bin/bash"
 
     # Download Burp Suite Profesional Latet Version
     echo 'Downloading Burp Suite Professional Latest....'
@@ -8,8 +7,6 @@ curl "https://portswigger-cdn.net/burp/releases/download?product=pro&version=&ty
     Link="https://portswigger-cdn.net/burp/releases/download?product=pro&version=&type=jar"
     echo $version
     wget "$Link" -O burpsuite_pro_v$version.jar --quiet --show-progress
-java -jar loader.jar &
-sleep 2s
 
     # Execute Keygenerator
     echo 'Starting Keygenerator'
@@ -17,7 +14,7 @@ sleep 2s
     sleep 2s
 
     # Execute Burp Suite Professional Latest with Key
-    echo "java --add-opens=java.desktop/javax.swing=ALL-UNNAMED--add-opens=java.base/java.lang=ALL-UNNAMED --add-opens=java.base/jdk.internal.org.objectweb.asm=ALL-UNNAMED --add-opens=java.base/jdk.internal.org.objectweb.asm.tree=ALL-UNNAMED --add-opens=java.base/jdk.internal.org.objectweb.asm.Opcodes=ALL-UNNAMED -javaagent:loader.jar -noverify -jar burpsuite_pro_latest.jar &" > burpsuite_pro_latest
+    echo "java --add-opens=java.desktop/javax.swing=ALL-UNNAMED--add-opens=java.base/java.lang=ALL-UNNAMED --add-opens=java.base/jdk.internal.org.objectweb.asm=ALL-UNNAMED --add-opens=java.base/jdk.internal.org.objectweb.asm.tree=ALL-UNNAMED --add-opens=java.base/jdk.internal.org.objectweb.asm.Opcodes=ALL-UNNAMED -javaagent:loader.jar -noverify -jar burpsuite_pro_v&version.jar &" > burpsuite_pro_latest
     chmod +x burpsuite_pro_latest
     cp burpsuite_pro_latest /bin/burpsuite_pro_latest
     ./burpsuite_pro_latest
