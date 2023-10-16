dda6ae0eaec82cd3f85fa3e12a59ecd9e6ecf444
xiv3r <vanniealdamar@gmail.com>
2023-10-16 09:37:30 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
     # Download BurpSuite Professional
-    echo 'Downloading Latest BurpSuite Professional...'
+    echo 'Downloading Latest BurpSuite Professional Latest...'
     html=$(curl -s https://portswigger.net/burp/releases)
     version=$(echo $html | grep -Po '(?<=/burp/releases/professional-community-)[0-9]+\-[0-9]+\-[0-9]+' | head -n 1)
     Link="https://portswigger-cdn.net/burp/releases/download?product=pro&version=&type=jar"
@@ -14,7 +14,7 @@
     sleep 2s
     
     # Execute BurpSuite Professional Latest with Key Unlocker
-    echo 'Executing Burp Suite Professional Latest with Keyloader'
+    echo 'Executing BurpSuite Professional Latest with Keyloader...'
     echo "java --add-opens=java.desktop/javax.swing=ALL-UNNAMED--add-opens=java.base/java.lang=ALL-UNNAMED --add-opens=java.base/jdk.internal.org.objectweb.asm=ALL-UNNAMED --add-opens=java.base/jdk.internal.org.objectweb.asm.tree=ALL-UNNAMED --add-opens=java.base/jdk.internal.org.objectweb.asm.Opcodes=ALL-UNNAMED -javaagent:$(pwd)/loader.jar -noverify -jar $(pwd)/burpsuite_pro_v$version.jar &" > BURPSUITE
     chmod +x BURPSUITE
     cp BURPSUITE /bin/BURPSUITE
