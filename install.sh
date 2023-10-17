6d3d4474431de228be32a2399e19b2dc9197af20
xiv3r <vanniealdamar@gmail.com>
2023-10-17 12:22:07 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,8 +2,8 @@
 
     # Download BurpSuite Professional
     echo 'Downloading BurpSuite Professional Latest...'
-    curl "https://portswigger-cdn.net/burp/releases/download?product=pro&version=&type=jar" -o BurpSuite-Professional-Latest.jar
-    
+    wget "https://portswigger-cdn.net/burp/releases/download?product=pro&version=&type=jar" -O BurpSuite-Professional-Latest.jar --quiet --show-progress
+
     # Execute Key Generator
     echo 'Starting Keygenerator'
     (java -jar loader.jar) &
@@ -11,7 +11,7 @@
     
     # Execute BurpSuite Professional Latest with Key Unlocker
     echo 'Executing BurpSuite Professional Latest with Keyloader...'
-    echo "java --add-opens=java.desktop/javax.swing=ALL-UNNAMED--add-opens=java.base/java.lang=ALL-UNNAMED --add-opens=java.base/jdk.internal.org.objectweb.asm=ALL-UNNAMED --add-opens=java.base/jdk.internal.org.objectweb.asm.tree=ALL-UNNAMED --add-opens=java.base/jdk.internal.org.objectweb.asm.Opcodes=ALL-UNNAMED -javaagent:$(pwd)/loader.jar -noverify -jar $(pwd)/BurpSuite-Professional-Latest.jar &" > BurpSuite-Professional-Latest
+    echo "java --add-opens=java.desktop/javax.swing=ALL-UNNAMED --add-opens=java.base/java.lang=ALL-UNNAMED --add-opens=java.base/jdk.internal.org.objectweb.asm=ALL-UNNAMED --add-opens=java.base/jdk.internal.org.objectweb.asm.tree=ALL-UNNAMED --add-opens=java.base/jdk.internal.org.objectweb.asm.Opcodes=ALL-UNNAMED -javaagent:$(pwd)/loader.jar -noverify -jar $(pwd)/BurpSuite-Professional-Latest.jar &" > BurpSuite-Professional-Latest
     chmod +x BurpSuite-Professional-Latest
     cp BurpSuite-Professional-Latest /bin/BurpSuite-Professional-Latest
     (./BurpSuite-Professional-Latest)
