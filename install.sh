d975ab3529e9e5e555f4f3e1fbf5e09c2ffce455
xiv3r <vanniealdamar@gmail.com>
2023-10-24 14:05:20 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,17 +1,21 @@
 #!/bin/bash
 
-    # Download BurpSuite Professional
-    echo 'Downloading BurpSuite Professional Latest...'
-    wget "https://portswigger-cdn.net/burp/releases/download?product=pro&version=&type=jar" -O BurpSuite-Professional-Latest.jar --quiet --show-progress
-
-    # Execute Key Generator
+    # Download Burpsuite Professional Latest.
+    echo 'Downloading Latest Burpsuite Professional Latest...'
+    html=$(curl -s https://portswigger.net/burp/releases)
+    version=$(echo $html | grep -Po '(?<=/burp/releases/professional-community-)[0-9]+\-[0-9]+\-[0-9]+' | head -n 1)
+    Link="https://portswigger-cdn.net/burp/releases/download?product=pro&version=&type=jar"
+    echo $version
+    wget "$Link" -O burpsuite_pro_v$version.jar --quiet --show-progress
+    
+    # Execute Key Generator.
     echo 'Starting Keygenerator'
     (java -jar loader.jar) &
     sleep 2s
-    
-    # Execute BurpSuite Professional Latest with Key Unlocker
-    echo 'Executing BurpSuite Professional Latest with Keyloader...'
-    echo "java --add-opens=java.desktop/javax.swing=ALL-UNNAMED --add-opens=java.base/java.lang=ALL-UNNAMED --add-opens=java.base/jdk.internal.org.objectweb.asm=ALL-UNNAMED --add-opens=java.base/jdk.internal.org.objectweb.asm.tree=ALL-UNNAMED --add-opens=java.base/jdk.internal.org.objectweb.asm.Opcodes=ALL-UNNAMED -javaagent:$(pwd)/loader.jar -noverify -jar $(pwd)/BurpSuite-Professional-Latest.jar &" > BurpSuite-Professional-Latest
-    chmod +x BurpSuite-Professional-Latest
-    cp BurpSuite-Professional-Latest /bin/BurpSuite-Professional-Latest
-    (./BurpSuite-Professional-Latest)
+
+    # Execute BurpSuite Professional Latest with License Key Loader.
+    echo 'Executing Burpsuite Professional Latest with License Key loader...'
+    echo "java --add-opens=java.desktop/javax.swing=ALL-UNNAMED--add-opens=java.base/java.lang=ALL-UNNAMED --add-opens=java.base/jdk.internal.org.objectweb.asm=ALL-UNNAMED --add-opens=java.base/jdk.internal.org.objectweb.asm.tree=ALL-UNNAMED --add-opens=java.base/jdk.internal.org.objectweb.asm.Opcodes=ALL-UNNAMED -javaagent:$(pwd)/loader.jar -noverify -jar $(pwd)/burpsuite_pro_v$version.jar &" > BURPSUITE
+    chmod +x BURPSUITE
+    cp BURPSUITE /bin/BURPSUITE
+    (./BURPSUITE)
