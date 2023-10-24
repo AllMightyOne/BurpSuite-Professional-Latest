3f6e017e8bf6eeb51df1bb0191856200977cd3af
xiv3r <vanniealdamar@gmail.com>
2023-10-24 14:10:06 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
     # Download Burpsuite Professional Latest.
-    echo 'Downloading Latest Burpsuite Professional Latest...'
+    echo 'Downloading Burpsuite Professional Latest...'
     html=$(curl -s https://portswigger.net/burp/releases)
     version=$(echo $html | grep -Po '(?<=/burp/releases/professional-community-)[0-9]+\-[0-9]+\-[0-9]+' | head -n 1)
     Link="https://portswigger-cdn.net/burp/releases/download?product=pro&version=&type=jar"
