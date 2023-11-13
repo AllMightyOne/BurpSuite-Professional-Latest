b8072a8bba9d8bc785dfe83cfebdbd4d65356a5a
xiv3r <vanniealdamar@gmail.com>
2023-11-14 06:29:04 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,6 +2,7 @@
 
     # Download Burpsuite Professional Latest.
     echo 'Downloading Burpsuite Professional Latest...'
+    
     html=$(curl -s https://portswigger.net/burp/releases)
     version=$(echo $html | grep -Po '(?<=/burp/releases/professional-community-)[0-9]+\-[0-9]+\-[0-9]+' | head -n 1)
     Link="https://portswigger-cdn.net/burp/releases/download?product=pro&version=&type=jar"
@@ -10,12 +11,13 @@
     
     # Execute Key Generator.
     echo 'Starting Keygenerator'
+    
     (java -jar loader.jar) &
     sleep 2s
 
-    # Execute BurpSuite Professional Latest with License Key Loader.
-    echo 'Executing Burpsuite Professional Latest with License Key loader...'
-    echo "java --add-opens=java.desktop/javax.swing=ALL-UNNAMED--add-opens=java.base/java.lang=ALL-UNNAMED --add-opens=java.base/jdk.internal.org.objectweb.asm=ALL-UNNAMED --add-opens=java.base/jdk.internal.org.objectweb.asm.tree=ALL-UNNAMED --add-opens=java.base/jdk.internal.org.objectweb.asm.Opcodes=ALL-UNNAMED -javaagent:$(pwd)/loader.jar -noverify -jar $(pwd)/burpsuite_pro_v$version.jar &" > BURPSUITE
-    chmod +x BURPSUITE
-    cp BURPSUITE /bin/BURPSUITE
-    (./BURPSUITE)
+ # Execute Burp Suite Professional with Keyloader
+    echo 'Executing Burp Suite Professional with Keyloader'
+    
+    echo "java --add-opens=java.desktop/javax.swing=ALL-UNNAMED--add-opens=java.base/java.lang=ALL-UNNAMED --add-opens=java.base/jdk.internal.org.objectweb.asm=ALL-UNNAMED --add-opens=java.base/jdk.internal.org.objectweb.asm.tree=ALL-UNNAMED --add-opens=java.base/jdk.internal.org.objectweb.asm.Opcodes=ALL-UNNAMED -javaagent:$(pwd)/loader.jar -noverify -jar $(pwd)/burpsuite_pro_v$version.jar &" > burpsuitepro
+    chmod 777 burpsuitepro
+    cp burpsuite /bin/burpsuitepro
