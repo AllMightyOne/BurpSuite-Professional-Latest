550a5ba12f80b63572df6a946063db48d463ff65
xiv3r <vanniealdamar@gmail.com>
2023-11-14 07:41:26 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,6 +3,14 @@
 # Download Burpsuite Professional Latest.
     echo 'Downloading Burpsuite Professional Latest...'
     
+    mkdir -p /usr/share/burpsuite
+    cp loader.jar /usr/share/burpsuite/
+    cp burp_suite.ico /usr/share/burpsuite/
+    rm install.ps1
+    rm -rf .git
+    cd /usr/share/burpsuite/
+	rm burpsuite.jar
+    
     html=$(curl -s https://portswigger.net/burp/releases)
     version=$(echo $html | grep -Po '(?<=/burp/releases/professional-community-)[0-9]+\-[0-9]+\-[0-9]+' | head -n 1)
     Link="https://portswigger-cdn.net/burp/releases/download?product=pro&version=&type=jar"
@@ -21,8 +29,7 @@
 # Execute Burp Suite Professional with Keyloader
     echo 'Executing BurpSuite Professional with Key Generator'
     
-   echo "java --add-opens=java.desktop/javax.swing=ALL-UNNAMED --add-opens=java.base/java.lang=ALL-UNNAMED --add-opens=java.base/jdk.internal.org.objectweb.asm=ALL-UNNAMED --add-opens=java.base/jdk.internal.org.objectweb.asm.tree=ALL-UNNAMED --add-opens=java.base/jdk.internal.org.objectweb.asm.Opcodes=ALL-UNNAMED -javaagent:$(pwd)/loader.jar -noverify -jar $(pwd)/burpsuite_pro_v$version.jar &" > burpsuitepro
-    
-    chmod 777 burpsuitepro
-    cp burpsuitepro /bin/burpsuitepro
-    (./burpsuitepro)
+   echo "java --add-opens=java.desktop/javax.swing=ALL-UNNAMED --add-opens=java.base/java.lang=ALL-UNNAMED --add-opens=java.base/jdk.internal.org.objectweb.asm=ALL-UNNAMED --add-opens=java.base/jdk.internal.org.objectweb.asm.tree=ALL-UNNAMED --add-opens=java.base/jdk.internal.org.objectweb.asm.Opcodes=ALL-UNNAMED -javaagent:$(pwd)/loader.jar -noverify -jar $(pwd)/burpsuite_pro_v$version.jar &" > burpsuite
+    chmod 777 burpsuite
+    cp burpsuitepro /bin/burpsuite
+    (./burpsuite)
