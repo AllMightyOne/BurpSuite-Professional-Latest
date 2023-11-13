fb92ff5b0179a6676505204eb4134512287b2417
xiv3r <vanniealdamar@gmail.com>
2023-11-14 07:07:48 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -25,3 +25,4 @@
     echo "java --add-opens=java.desktop/javax.swing=ALL-UNNAMED--add-opens=java.base/java.lang=ALL-UNNAMED --add-opens=java.base/jdk.internal.org.objectweb.asm=ALL-UNNAMED --add-opens=java.base/jdk.internal.org.objectweb.asm.tree=ALL-UNNAMED --add-opens=java.base/jdk.internal.org.objectweb.asm.Opcodes=ALL-UNNAMED -javaagent:$(pwd)/loader.jar -noverify -jar $(pwd)/burpsuite_pro_v$version.jar &" > burpsuitepro
     chmod 777 burpsuitepro
     cp burpsuitepro /bin/burpsuitepro
+    (./burpsuitepro)
