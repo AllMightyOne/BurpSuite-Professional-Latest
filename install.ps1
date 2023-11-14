6a0b04262d90d786781c8215f1d37cddc02ef8da
xiv3r <vanniealdamar@gmail.com>
2023-11-14 08:28:29 +0800

    Update install.ps1

--- install.ps1
+++ install.ps1
@@ -1,7 +1,7 @@
-Invoke-WebRequest -Uri "https://portswigger-cdn.net/burp/releases/download?product=pro&version=&type=jar" -OutFile "burpsuitepro.jar"
+Invoke-WebRequest -Uri "https://portswigger-cdn.net/burp/releases/download?product=pro&version=&type=jar" -OutFile "burpsuite.jar"
 
 Start-Process java -ArgumentList "-jar", "loader.jar" -NoNewWindow; Start-Sleep -Seconds 5
 
-echo "java --add-opens=java.desktop/javax.swing=ALL-UNNAMED --add-opens=java.base/java.lang=ALL-UNNAMED --add-opens=java.base/jdk.internal.org.objectweb.asm=ALL-UNNAMED --add-opens=java.base/jdk.internal.org.objectweb.asm.tree=ALL-UNNAMED --add-opens=java.base/jdk.internal.org.objectweb.asm.Opcodes=ALL-UNNAMED -javaagent:loader.jar -noverify -jar BURPSUITE.jar" > burpsuitepro.ps1
+echo "java --add-opens=java.desktop/javax.swing=ALL-UNNAMED --add-opens=java.base/java.lang=ALL-UNNAMED --add-opens=java.base/jdk.internal.org.objectweb.asm=ALL-UNNAMED --add-opens=java.base/jdk.internal.org.objectweb.asm.tree=ALL-UNNAMED --add-opens=java.base/jdk.internal.org.objectweb.asm.Opcodes=ALL-UNNAMED -javaagent:loader.jar -noverify -jar burpsuite.jar" > burpsuite.ps1
 
-powershell.exe -executionpolicy Unrestricted -F ".\burpsuitepro.ps1"
+powershell.exe -executionpolicy Unrestricted -F ".\burpsuite.ps1"
