84427a980b0075cb9f0605d867644dd83681c6bb
xiv3r <vanniealdamar@gmail.com>
2023-11-20 08:40:18 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,13 +2,11 @@
 
 # Download Burpsuite Professional Latest.
     echo 'Downloading Burpsuite Professional Latest...'
-    mkdir -p /usr/share/burpsuite
-    cp loader.jar /usr/share/burpsuite/
-    cp burp_suite.ico /usr/share/burpsuite/
+    mkdir -p /usr/share/burpsuitepro
+    cp loader.jar /usr/share/burpsuitepro/
+    cp burp_suite.ico /usr/share/burpsuitepro/
     rm install.ps1
     rm -rf .git
-    cd /usr/share/burpsuite/
-    rm burpsuite.jar
     html=$(curl -s https://portswigger.net/burp/releases)
     version=$(echo $html | grep -Po '(?<=/burp/releases/professional-community-)[0-9]+\-[0-9]+\-[0-9]+' | head -n 1)
     Link="https://portswigger-cdn.net/burp/releases/download?product=pro&version=&type=jar"
@@ -20,9 +18,9 @@
     (java -jar loader.jar) &
     sleep 2s
 
-# Execute Burp Suite Professional with Keyloader
-    echo 'Executing BurpSuite Professional with Key Generator'
-    echo "java --add-opens=java.desktop/javax.swing=ALL-UNNAMED --add-opens=java.base/java.lang=ALL-UNNAMED --add-opens=java.base/jdk.internal.org.objectweb.asm=ALL-UNNAMED --add-opens=java.base/jdk.internal.org.objectweb.asm.tree=ALL-UNNAMED --add-opens=java.base/jdk.internal.org.objectweb.asm.Opcodes=ALL-UNNAMED -javaagent:$(pwd)/loader.jar -noverify -jar $(pwd)/burpsuite_pro_v$version.jar &" > burpsuite
-    chmod 777 burpsuite
-    cp burpsuite /bin/burpsuite
-    (./burpsuite)
+# Execute Burp Suite Professional
+    echo 'Executing Burpsuite Professional with Key Generator'
+    echo "java --add-opens=java.desktop/javax.swing=ALL-UNNAMED --add-opens=java.base/java.lang=ALL-UNNAMED --add-opens=java.base/jdk.internal.org.objectweb.asm=ALL-UNNAMED --add-opens=java.base/jdk.internal.org.objectweb.asm.tree=ALL-UNNAMED --add-opens=java.base/jdk.internal.org.objectweb.asm.Opcodes=ALL-UNNAMED -javaagent:$(pwd)/loader.jar -noverify -jar $(pwd)/burpsuite_pro_v$version.jar &" > burpsuitepro
+    chmod 777 burpsuitepro
+    cp burpsuitepro /bin/burpsuitepro
+    (./burpsuitepro)
