243003bc6663125ca274bda4a138141c77e20cde
xiv3r <vanniealdamar@gmail.com>
2024-01-22 15:26:12 +0800

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
-   echo 'Updating Burpsuite Professional...'
+   echo 'Removing Old Files...'
    rm -rf /home/*/BurpSuite-Professional-Latest
    rm -rf /usr/share/burpsuitepro
    rm -rf /bin/burpsuitepro
-   
-   git clone https://github.com/xiv3r/BurpSuite-Professional-Latest.git
+
+   sleep 1s &
+   echo 'Downloading Burpsuite Professional...'
+   git clone https://github.com/xiv3r/BurpSuite-Professional-Late>
    cd BurpSuite-Professional-Latest
    chmod +x install.sh
    bash install.sh
